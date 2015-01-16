17fc2663f71a1d1ddf4b50b5a93add90337cfa4d
Antonio Coppola <acoppola@college.harvard.edu>
2015-01-16 14:57:19 +0100

    Further memory management cleanup

--- js/slider_control.js
+++ js/slider_control.js
@@ -1,24 +1,29 @@
-var length_refresh = function () {
-	
-	// Update scale_param
-	scale_param = 31 - slider.getValue();
+var length_refresh = function() {
 
-	// Update mapOptions
-	mapOptions.zoom = map.zoom;
-	mapOptions.center = map.center;
-	mapOptions.mapTypeId = map.mapTypeId;
+    // Update scale_param
+    scale_param = 31 - slider.getValue();
 
-	// Update map
-	map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
-	for (var key in Object.keys(data_files)){
-		drawArrows(data_files[Object.keys(data_files)[key]], scale_param, 'brown');
-	}
+    // Update mapOptions
+    mapOptions.zoom = map.zoom;
+    mapOptions.center = map.center;
+    mapOptions.mapTypeId = map.mapTypeId;
+
+    // Clear lines array
+    while (lines.length > 0) {
+        lines.pop();
+    }
+
+    // Update map
+    map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+    for (var key in Object.keys(data_files)) {
+        drawArrows(data_files[Object.keys(data_files)[key]], scale_param, 'brown');
+    }
 };
 
 var slider = $('.slider_bs').slider({
         formatter: function(value) {
-          return 'Current value: ' + value;
+            return 'Current value: ' + value;
         }
-      })
+    })
     .on('slideStop', length_refresh)
     .data('slider');
